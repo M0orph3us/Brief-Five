b88b7d616fde9e9b31e0dea8615f098eb73c1f83
Gael <gael.moreau.simplon@gmail.com>
2024-03-18 17:01:59 +0100

    push before leave the school

--- Backend/models/repositories/UsersRepository.php
+++ Backend/models/repositories/UsersRepository.php
@@ -1,15 +1,62 @@
 <?php
+
 require '../../config/autoload.php';
+
 final class UsersRepository
 {
     // params
+    private $db;
 
     // contructor
     public function __construct()
     {
-        $db = new Database();
-        $getDb = $db->getConnection();
+        $this->db = new Database();
+        $this->db = $this->db->getConnection();
     }
 
     // CRUD
+
+    public function create(array $data)
+    {
+        $sql = "INSERT INTO Users (firtsname, lastname, mail, password, createdAt) VALUE(firtsname:, lastname:, mail:, password:, createdAt:)";
+        try {
+
+            $params = [
+                'firtsname' => $data['firstnme'],
+                'lastname' => $data['lastname'],
+                'mail' => $data['mail'],
+                'password' => $data['password'],
+                'createdAt' => date('d/m/Y')
+            ];
+
+            $stmt = $this->db->prepare($sql);
+            $stmt->execute($params);
+            $stmt->closeCursor();
+
+            $userId = $this->db->lastInsertId();
+            $params['id'] = $userId;
+            $user = new Users($params);
+            return $user;
+        } catch (PDOException $e) {
+            echo 'Error : ' . $e->getMessage();
+        }
+    }
+
+    public function readOne()
+    {
+    }
+
+    public function readAll()
+    {
+        $sql = "SELECT * FROM users";
+        $stmt = $this->db->prepare($sql);
+    }
+
+    public function update()
+    {
+    }
+
+    public function delete()
+    {
+    }
 }
